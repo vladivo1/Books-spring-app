87be0424ed4144d97a1157d3b644f0e68cbf9fa7
vladivo1 <vla.ivolga@gmail.com>
2022-02-18 00:26:49 +0200

    added RestApiResponseHandler

--- src/main/java/com/example/springbootapp/response/RestApiResponse.java
+++ src/main/java/com/example/springbootapp/response/RestApiResponse.java
@@ -1,19 +1,31 @@
 package com.example.springbootapp.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RestApiResponse {
-    private int status;
-    private String message;
-    private Object data;
+import java.util.HashMap;
+import java.util.Map;
 
-    public RestApiResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
+@ControllerAdvice
+public class RestApiResponse extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected @NotNull ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName,message);
+
+        });
+        return new ResponseEntity<Object>(errors,HttpStatus.BAD_REQUEST);
     }
 }
